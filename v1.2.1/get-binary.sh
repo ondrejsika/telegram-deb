1f36e3f93242a103a35d54cc1fe14ca84ddb6016
Ondrej Sika <ondrej@ondrejsika.com>
2018-02-28 16:48:52 +0100

    fix v1.2.1/get-binary.sh - create /usr/bin dir

--- v1.2.1/get-binary.sh
+++ v1.2.1/get-binary.sh
@@ -2,6 +2,7 @@
 
 wget https://github.com/telegramdesktop/tdesktop/releases/download/v1.2.1/tsetup.1.2.1.tar.xz
 tar -xf tsetup.1.2.1.tar.xz
+mkdir -p telegram-1.2.1-ondrejsika-amd64/usr/bin
 cp Telegram/Telegram telegram-1.2.1-ondrejsika-amd64/usr/bin/telegram
 rm -rf tsetup.1.2.1.tar.xz Telegram
 
