7714f3f3f05b5a1fc2efb69851bb611ca41a2a7a
Ondrej Sika <ondrej@ondrejsika.com>
2019-03-19 14:41:16 +0100

    add v1.6.0

---
+++ v1.6.0/get-binary.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+VERSION=1.6.0
+
+wget https://github.com/telegramdesktop/tdesktop/releases/download/v$VERSION/tsetup.$VERSION.tar.xz
+tar -xf tsetup.$VERSION.tar.xz
+mkdir -p telegram-$VERSION-ondrejsika-amd64/usr/bin
+cp Telegram/Telegram telegram-$VERSION-ondrejsika-amd64/usr/bin/telegram
+rm -rf tsetup.$VERSION.tar.xz Telegram
+
